d3fe3f0106c41afef1e97beb0af8117691cfdcf1
John Owen <johnowen357@gmail.com>
2017-06-09 17:10:33 +1000

    Updated example code to reflect recent commits

--- example/entry.js
+++ example/entry.js
@@ -1,230 +1,264 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import equal from 'deep-is';
 import TableDragSelect from '../src';
-import '../table-drag-select.css';
-import './style.css';
+import '../style.css'; // Global styles
+import './style.css'; // Example code styles
 
 const jsCode = `import React from 'react';
 import ReactDOM from 'react-dom';
 import TableDragSelect from 'react-table-drag-select';
+import 'react-table-drag-select/style.css';
+// Assumes your using webpack, css-loader, and style-loader, if not, just copy
+// the stylesheet into your project and use a <link> tag in your HTML
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      model: new TableDragSelect.Model()
+    };
+    this.handleModelChange = this.handleModelChange.bind(this);
+    this.resetTable = this.resetTable.bind(this);
+  }
+
   return (
-    <TableDragSelect
-      onChange={e => ... /* Your handler goes here */}
-      clearRef={clear => ... /* clear() clears all cells */}
-    >
-      <tr>
-        <td disabled />
-        <td disabled>Monday</td>
-        <td disabled>Tuesday</td>
-        <td disabled>Wednesday</td>
-        <td disabled>Thursday</td>
-        <td disabled>Friday</td>
-        <td disabled>Saturday</td>
-      </tr>
-      <tr>
-        <td disabled>10:00</td>
-        <td />
-        <td />
-        <td />
-        <td />
-        <td />
-        <td>overtime</td>
-      </tr>
-      <tr>
-        <td disabled>11:00</td>
-        <td />
-        <td />
-        <td />
-        <td />
-        <td />
-        <td>overtime</td>
-      </tr>
-      <tr>
-        <td disabled>12:00</td>
-        <td />
-        <td />
-        <td />
-        <td />
-        <td />
-        <td>overtime</td>
-      </tr>
-      <tr>
-        <td disabled>13:00</td>
-        <td />
-        <td />
-        <td />
-        <td />
-        <td />
-        <td>overtime</td>
-      </tr>
-      <tr>
-        <td disabled>14:00</td>
-        <td />
-        <td />
-        <td />
-        <td />
-        <td />
-        <td>overtime</td>
-      </tr>
-      <tr>
-        <td disabled>15:00</td>
-        <td />
-        <td />
-        <td />
-        <td />
-        <td />
-        <td>overtime</td>
-      </tr>
-      <tr>
-        <td disabled>16:00</td>
-        <td />
-        <td />
-        <td />
-        <td />
-        <td />
-        <td>overtime</td>
-      </tr>
-    </TableDragSelect>
+    <div>
+      <h2>Timetable</h2>
+      <TableDragSelect
+        model={this.state.model}
+        onModelChange={this.handleModelChange}
+      >
+        <tr>
+          <td disabled />
+          <td disabled>Monday</td>
+          <td disabled>Tuesday</td>
+          <td disabled>Wednesday</td>
+          <td disabled>Thursday</td>
+          <td disabled>Friday</td>
+          <td disabled>Saturday</td>
+        </tr>
+        <tr>
+          <td disabled>10:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>11:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>12:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>13:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>14:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>15:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>16:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+      </TableDragSelect>
+      <button onClick={resetTable}>Reset</Reset>
+      <h2>onModelChange callback</h2>
+      <pre>
+        model.getCellsSelected() === {JSON.stringify(this.state.model.getCellsSelected())}
+      </pre>
+  </div>
   );
+
+  handleModelChange(model) {
+    this.setState({model});
+  }
+
+  resetTable() {
+    let model = this.state.model.clone();
+    model.clear();
+    this.setState({model});
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('timetable'));`;
 
+const cssCode = `/* Cells that can be interacted with */
+.table-drag-select td.cell-enabled {
+  color: #806E52;
+  background-color: #FBF1E8;
+}
+
+/* Cells that cannot be interacted with */
+.table-drag-select td.cell-disabled {
+  /* Put yer css here */
+}
+
+/* Cells that have been selected */
+.table-drag-select td.cell-selected {
+  background-color: #FBD5D4;
+}
+
+/* Cells that are in the process of being selected */
+.table-drag-select td.cell-being-selected {
+  background-color: #EACA96;
+}
+
+/* Cells in first row */
+.table-drag-select tr:first-child td {
+  /* Remove empty space at top of table */
+  border-top: 0;
+  line-height: 1rem;
+}
+
+/* Cells in first column */
+.table-drag-select td:first-child {
+  /* Consume less whitespace */
+  width: 3rem;
+}`;
+
 const resultingDOM = `<div id="timetable">
-  <table className="table-drag-select">
+  <h2>Timetable</h2>
+  <table class="table-drag-select">
     <tbody>
       <tr>
-        <td className="cell-disabled" />
-        <td className="cell-disabled">Monday</td>
-        <td className="cell-disabled">Tuesday</td>
-        <td className="cell-disabled">Wednesday</td>
-        <td className="cell-disabled">Thursday</td>
-        <td className="cell-disabled">Friday</td>
-        <td className="cell-disabled">Saturday</td>
+        <td class="cell-disabled" />
+        <td class="cell-disabled">Monday</td>
+        <td class="cell-disabled">Tuesday</td>
+        <td class="cell-disabled">Wednesday</td>
+        <td class="cell-disabled">Thursday</td>
+        <td class="cell-disabled">Friday</td>
+        <td class="cell-disabled">Saturday</td>
       </tr>
       <tr>
-        <td className="cell-disabled">10:00</td>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled">overtime</td>
+        <td class="cell-disabled">10:00</td>
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled">overtime</td>
       </tr>
       <tr>
-        <td className="cell-disabled">11:00</td>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled">overtime</td>
+        <td class="cell-disabled">11:00</td>
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled">overtime</td>
       </tr>
       <tr>
-        <td className="cell-disabled">12:00</td>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled">overtime</td>
+        <td class="cell-disabled">12:00</td>
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled">overtime</td>
       </tr>
       <tr>
-        <td className="cell-disabled">13:00</td>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled">overtime</td>
+        <td class="cell-disabled">13:00</td>
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled">overtime</td>
       </tr>
       <tr>
-        <td className="cell-disabled">14:00</td>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled">overtime</td>
+        <td class="cell-disabled">14:00</td>
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled">overtime</td>
       </tr>
       <tr>
-        <td className="cell-disabled">15:00</td>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled">overtime</td>
+        <td class="cell-disabled">15:00</td>
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled">overtime</td>
       </tr>
       <tr>
-        <td className="cell-disabled">16:00</td>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled"/>
-        <td className="cell-enabled">overtime</td>
+        <td class="cell-disabled">16:00</td>
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled" />
+        <td class="cell-enabled">overtime</td>
       </tr>
     </tbody>
   </table>
-</div>
-`;
-
-const cssCode = `<link rel="stylesheet" href="table-drag-select.css">
-<style>
-  /* Cells that can be interacted with */
-  .table-drag-select td.cell-enabled {
-    color: #806E52;
-    background-color: #FBF1E8;
-  }
-  
-  /* Cells that cannot be interacted with */
-  .table-drag-select td.cell-disabled {
-    /* Put yer css here */
-  }
-  
-  /* Cells that have been selected */
-  .table-drag-select td.cell-selected {
-    background-color: #FBD5D4;
-  }
-  
-  /* Cells that are in the process of being selected */
-  .table-drag-select td.cell-being-selected {
-    background-color: #EACA96;
-  }
-  
-  /* Cells in first row */
-  .table-drag-select tr:first-child td {
-    /* Remove empty space at top of table */
-    border-top: 0;
-    line-height: 1rem;
-  }
-  
-  /* Cells in first column */
-  .table-drag-select td:first-child {
-    /* Consume less whitespace */
-    width: 3rem;
-  }
-</style>`;
+  <button>Reset</Reset>
+  <h2>onModelChange callback</h2>
+  <pre>
+    model.getCellsSelected() === [
+      [false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false]
+    ]
+  </pre>
+</div>`;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cellsSelected: [
-        [false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false]
-      ] 
+      model: new TableDragSelect.Model(8, 7)
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   render() {
@@ -238,8 +272,8 @@ class App extends React.Component {
         <h2>Timetable</h2>
         <div className="table-container">
           <TableDragSelect
-            onChange={this.handleChange}
-            clearRef={clear => this.clearTable = clear}
+            model={this.state.model}
+            onModelChange={this.handleChange}
           >
             <tr>
               <td disabled />
@@ -315,24 +349,40 @@ class App extends React.Component {
             </tr>
           </TableDragSelect>
         </div>
-        <button onClick={e => this.clearTable()}>Reset</button>
-        <h2><code>{'onChange={e => ...}'}</code> callback</h2>
+        <button onClick={this.handleReset}>Reset</button>
+        <h2><code>{'onModelChange={model => ...}'}</code> callback</h2>
         <pre ref="output">
-          e.cellsSelected = {stringifyBoolMatrix(this.state.cellsSelected)}
+          model.getCellsSelected() === {stringifyBoolMatrix(this.state.model.getCellsSelected())}
         </pre>
-        <h2>JS code</h2>
+        <h2><code>script.js</code></h2>
         <pre>{jsCode}</pre>
+        <h2><code>style.css</code></h2>
+        <pre>{cssCode}</pre>
         <h2>Resulting DOM</h2>
         <pre>{resultingDOM}</pre>
-        <h2>CSS</h2>
-        <pre>{cssCode}</pre>
       </div>
     );
   }
 
-  handleChange(e) {
-    this.setState({cellsSelected: e.cellsSelected});
-    // Restart flash animation
+  handleChange(model) {
+    let currentCells = this.state.model.getCellsSelected();
+    if (!equal(currentCells, model.getCellsSelected())) {
+      this.restartFlashAnimation();
+    }
+    this.setState({model});
+  }
+
+  handleReset() {
+    let model = this.state.model.clone();
+    model.clear();
+    let currentCells = this.state.model.getCellsSelected();
+    if (!equal(currentCells, model.getCellsSelected())) {
+      this.restartFlashAnimation();
+    }
+    this.setState({model});
+  }
+
+  restartFlashAnimation() {
     this.refs.output.classList.remove('flash');
     setTimeout(() => this.refs.output.classList.add('flash'), 50);
   }
