73313d1b9288503641fac79bf0e2d30a77664f7e
John Owen <john.owen@aginicx.com>
2017-08-16 19:29:38 +1000

    <td>s can now contain block level elements

--- src/eventToCellLocation.js
+++ src/eventToCellLocation.js
@@ -6,12 +6,16 @@
 function eventToCellLocation(e) {
   let target;
   // For touchmove and touchend events, e.target and e.touches[n].target are
-  // wrong, so we have to rely on elementFromPoint()
+  // wrong, so we have to rely on elementFromPoint(). For mouse clicks, we have
+  // to use e.target.
   if (e.touches) {
     const touch = e.touches[0];
     target = document.elementFromPoint(touch.clientX, touch.clientY);
   } else {
     target = e.target;
+    while (target.tagName !== "TD") {
+      target = target.parentNode;
+    }
   }
   return getCellLocation(target);
 }
